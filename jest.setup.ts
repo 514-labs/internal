b19153f3de238e739a8dc265fcf5904e0e2806b3
Tim Delisle <tim@fiveonefour.com>
2025-10-10 14:20:26 -0700

    fixed build

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,4 +4,4 @@ import "@testing-library/jest-dom";
 process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY =
   "pk_test_dGVzdGluZy1jbGVyay1kb21haW4=";
 process.env.CLERK_SECRET_KEY = "sk_test_mock_secret_key";
-process.env.NODE_ENV = "test";
+// NOTE: NODE_ENV is set by Jest automatically and cannot be overridden
