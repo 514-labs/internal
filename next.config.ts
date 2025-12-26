a3d68e16ddcf80c2f40ebeaedb73de2a64a579fd
Tim Delisle <tim@fiveonefour.com>
2025-12-26 10:25:16 -0800

    simplify nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
 import type { NextConfig } from "next";
 import { baseURL } from "./baseUrl";
-import path from "path";
 
 const nextConfig: NextConfig = {
   assetPrefix: baseURL,
-  // Transpile the Mercury SDK from .api folder
-  transpilePackages: ["@api/mercurytechnologies"],
-  webpack: (config) => {
-    // Add the .api folder's node_modules to module resolution
-    config.resolve.modules = [
-      ...(config.resolve.modules || []),
-      path.resolve(__dirname, ".api/apis/mercurytechnologies/node_modules"),
-      "node_modules",
-    ];
-    return config;
-  },
 };
 
 export default nextConfig;
