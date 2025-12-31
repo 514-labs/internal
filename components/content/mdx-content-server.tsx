e941e906275eb6bb3add5463d8d4d1914d9804e3
Tim Delisle <tim@fiveonefour.com>
2025-12-31 11:31:17 -0800

    product write up

--- components/content/mdx-content-server.tsx
+++ components/content/mdx-content-server.tsx
@@ -14,6 +14,7 @@ import { CodeBlock } from "./code-block";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Item,
   ItemContent,
@@ -380,12 +381,19 @@ const mdxComponents = {
   Grid,
   LinkButton,
   Badge,
+  Alert,
+  AlertTitle,
+  AlertDescription,
   Card,
   CardContent,
   CardHeader,
   CardTitle,
   CardDescription,
   Button,
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
   CardGroup,
   ContentCard,
   // Item components
